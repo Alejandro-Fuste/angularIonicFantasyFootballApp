4374ad80236b5e3a959d6474fc4f74db6121e7be
Alejandro F <afuste7@gmail.com>
2023-06-15 15:26:06 -0400

    renamed tab1 folder to grades and made corresponding changes

--- src/app/grades/grades.page.ts
+++ src/app/grades/grades.page.ts
@@ -3,9 +3,9 @@ import { IonicModule } from '@ionic/angular';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss'],
+  selector: 'app-grades',
+  templateUrl: 'grades.page.html',
+  styleUrls: ['grades.page.scss'],
   standalone: true,
   imports: [IonicModule, ExploreContainerComponent],
 })
