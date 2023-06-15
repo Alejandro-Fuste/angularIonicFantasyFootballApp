d8759e5c1d015d56c0375e397b4eb201d2c46e16
Alejandro F <afuste7@gmail.com>
2023-06-15 14:42:29 -0400

    added initial ionic project files

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'fantasyApp',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
