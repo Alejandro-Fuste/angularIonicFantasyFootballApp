ef0ea460bc7e60fa9494ea6da4d14ef87923b667
Alejandro F <afuste7@gmail.com>
2023-06-19 17:11:12 -0400

    removed sum test files and added code for players tests. also added properties for player model

---
+++ src/app/grades/player.model.ts
@@ -0,0 +1,14 @@
+export class Player {
+  constructor(
+    public firstName: string,
+    public lastName: string,
+    public position: string,
+    public team: string,
+    public age: number,
+    public height: string,
+    public weight: string,
+    public yearsExp: number,
+    public depthChartOrder: number,
+    public grades: object
+  ) {}
+}
