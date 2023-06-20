134a95ddb2ed81f0daadf4f9d112d5da42043933
Alejandro F <afuste7@gmail.com>
2023-06-20 12:03:45 -0400

    added test descriptions for properties and subproperties

---
+++ test/grade/grade.test.ts
@@ -0,0 +1,76 @@
+import { describe, expect, test } from '@jest/globals';
+import { Grade } from '../../src/app/grades/grade.model';
+
+const g = {};
+
+describe('grade model', () => {
+  test('can instantiate grade instance', () => {
+    expect(typeof g).toBe('object');
+  });
+
+  test('grade object has overall property', () => {});
+
+  // main property tests
+
+  test('grade object has talent property', () => {});
+
+  test('grade object has offense property', () => {});
+
+  test('grade object has opportunity property', () => {});
+
+  test('grade object has production property', () => {});
+
+  // talent subproperty tests
+
+  test('talent property has velocity property', () => {});
+
+  test('talent property has acceleration property', () => {});
+
+  test('talent property has agility property', () => {});
+
+  test('talent property has age property', () => {});
+
+  test('talent property has size property', () => {});
+
+  test('talent property has skills property', () => {});
+
+  // skills subproperty tests for QB
+
+  test('skill property has mechanics & arm strength property', () => {});
+
+  test('skill property has protections property', () => {});
+
+  test('skill property has pocket awareness property', () => {});
+
+  test('skill property has decision making property', () => {});
+
+  test('skill property has defense recognition/turnover property', () => {});
+
+  test('skill property has precision property', () => {});
+
+  test('skill property has pressure performance property', () => {});
+
+  test('skill property has athleticism property', () => {});
+
+  // skills subproperty tests for RB
+
+  test('skill property has vision property', () => {});
+
+  test('skill property has gap, zone, both property', () => {});
+
+  test('skill property has receiving property', () => {});
+
+  test('skill property has blocking making property', () => {});
+
+  // skills subproperty tests for WR/TE
+
+  test('skill property has release property', () => {});
+
+  test('skill property has separation property', () => {});
+
+  test('skill property has route tree property', () => {});
+
+  test('skill property has hands making property', () => {});
+
+  test('skill property has blocking making property', () => {});
+});
