44d3c9e43a404521bd9cf0a880868c6d437157b4
Alejandro F <afuste7@gmail.com>
2023-06-21 21:56:16 -0400

    added properties and comments to grades model

--- src/app/grades/grade.model.ts
+++ src/app/grades/grade.model.ts
@@ -1,3 +1,29 @@
 export class Grade {
-  constructor() {}
+  constructor(
+    velocity: number,
+    accelartion: number,
+    agility: number,
+    age: number,
+    height: string,
+    weight: string,
+    skill_points: number
+  ) {}
 }
+
+// height grade method -> converts height input to points
+
+// weight grade method -> converts weight input to points
+
+/*
+
+sum points method -> takes in array of points for a criteria (talent, opportunity, offense, production)
+and returns a number representing the sum
+
+*/
+
+/*
+
+letter grade method -> takes the sum for a criteria (talent, opportunity, offense, production)
+and divides it by a total points input and returns the letter grade and percentage
+
+*/
