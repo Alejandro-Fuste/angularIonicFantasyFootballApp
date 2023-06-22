b14d958054cea422f4e0ec12de9465bb4a59e8bc
Alejandro F <afuste7@gmail.com>
2023-06-21 21:24:04 -0400

    added folders for rookie and non-rookie in test folder. Created separate test files for rookies and non-rookies.

--- test/grade/non-rookie/non-rookie-property-grade.test.ts
+++ test/grade/non-rookie/non-rookie-property-grade.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from '@jest/globals';
-import { Grade } from '../../src/app/grades/grade.model';
+import { Grade } from '../../../src/app/grades/grade.model';
 
 const g = {};
 
@@ -73,4 +73,38 @@ describe('grade model', () => {
   test('skill property has hands making property', () => {});
 
   test('skill property has blocking making property', () => {});
+
+  // offense criteria property tests
+
+  test('offense criteria property has quarterback property', () => {});
+
+  test('offense criteria property has offensive coordinator property', () => {});
+
+  test('offense criteria property has offensive line property', () => {});
+
+  test('offense criteria total grade methods returns a number grade', () => {});
+
+  // opportunity property tests
+
+  test('opportunity criteria property has games subproperty', () => {});
+
+  test('opportunity criteria property has snaps subproperty', () => {});
+
+  test('opportunity criteria property has carries subproperty', () => {});
+
+  test('opportunity criteria property has targets subproperty', () => {});
+
+  test('opportunity criteria property has receptions subproperty', () => {});
+
+  // production property tests
+
+  test('production criteria property has games subproperty', () => {});
+
+  test('production criteria property has snaps subproperty', () => {});
+
+  test('production criteria property has carries subproperty', () => {});
+
+  test('production criteria property has targets subproperty', () => {});
+
+  test('production criteria property has receptions subproperty', () => {});
 });
