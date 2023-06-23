77f62a7a559f10d023a29f67a45ffdacbf9831b3
Alejandro F <afuste7@gmail.com>
2023-06-23 08:49:51 -0400

    added tests outlines for rookie only

--- test/grade/rookie/rookie-only.test.ts
+++ test/grade/rookie/rookie-only.test.ts
@@ -1,90 +1,15 @@
 import { describe, expect, test } from '@jest/globals';
-import { Grade } from '../../../src/app/grades/grade.model';
 
-const g = {};
-
-describe('grade model', () => {
-  test('can instantiate grade instance', () => {
-    expect(typeof g).toBe('object');
-  });
-
-  test('grade object has overall property', () => {});
-
-  // main property tests
-
-  test('grade object has talent property', () => {});
-
-  test('grade object has offense property', () => {});
-
+describe('grade model tests that only apply for rookies', () => {
   test('grade object has opportunity property', () => {});
 
-  test('grade object has production property', () => {});
+  test('grade object has depth chart subproperty', () => {});
 
-  // talent subproperty tests
+  test('grade model depth chart method returns a grade for a player first in the depth chart', () => {});
 
-  test('talent property has velocity property', () => {});
+  test('grade model depth chart method returns a grade for a player third in the depth chart', () => {});
 
-  test('talent property has acceleration property', () => {});
+  test('grade model depth chart method returns a grade for a player last in the depth chart', () => {});
 
-  test('talent property has agility property', () => {});
-
-  test('talent property has age property', () => {});
-
-  test('talent property has size property', () => {});
-
-  test('talent property has skills property', () => {});
-
-  // skills subproperty tests for QB
-
-  test('skill property has mechanics & arm strength property', () => {});
-
-  test('skill property has protections property', () => {});
-
-  test('skill property has pocket awareness property', () => {});
-
-  test('skill property has decision making property', () => {});
-
-  test('skill property has defense recognition/turnover property', () => {});
-
-  test('skill property has precision property', () => {});
-
-  test('skill property has pressure performance property', () => {});
-
-  test('skill property has athleticism property', () => {});
-
-  // skills subproperty tests for RB
-
-  test('skill property has vision property', () => {});
-
-  test('skill property has gap, zone, both property', () => {});
-
-  test('skill property has receiving property', () => {});
-
-  test('skill property has blocking making property', () => {});
-
-  // skills subproperty tests for WR/TE
-
-  test('skill property has release property', () => {});
-
-  test('skill property has separation property', () => {});
-
-  test('skill property has route tree property', () => {});
-
-  test('skill property has hands making property', () => {});
-
-  test('skill property has blocking making property', () => {});
-
-  // offense criteria property tests
-
-  test('offense criteria property has quarterback property', () => {});
-
-  test('offense criteria property has offensive coordinator property', () => {});
-
-  test('offense criteria property has offensive line property', () => {});
-
-  test('offense criteria total grade methods returns a number grade', () => {});
-
-  // opportunity property tests
-
-  test('offense criteria property has quarterback property', () => {});
+  test('grade model depth chart method returns a grade for a player with no rank in the depth chart', () => {});
 });
