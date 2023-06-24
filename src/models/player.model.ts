9e35ff29b1941daecf22c9e9d3593f4edc0b1cff
Alejandro F <afuste7@gmail.com>
2023-06-24 14:44:48 -0400

    added this to class

--- src/models/player.model.ts
+++ src/models/player.model.ts
@@ -11,5 +11,17 @@ export class Player {
     public yearsExp: number,
     public depthChartOrder: number,
     public grades: object
-  ) {}
+  ) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.position = position;
+    this.team = team;
+    this.age = age;
+    this.height = height;
+    this.weight = weight;
+    this.yearsExp = yearsExp;
+    this.depthChartOrder = depthChartOrder;
+    this.grades = grades;
+  }
 }
