828c062a4288dac189cf2d2a6341b6600f59109b
Alejandro F <afuste7@gmail.com>
2023-06-24 12:26:22 -0400

    added tests for talent methods file and added test file for skill

---
+++ test/grade/talent/talent-method.test.ts
@@ -0,0 +1,30 @@
+import { describe, expect, test } from '@jest/globals';
+
+describe('grade model tests for talent methods', () => {
+  test('grade model can generate a grade for overall talent', () => {});
+
+  // velocity tests
+  test('velocity method should return 75 points for a 4.28 input', () => {});
+
+  test('velocity method should return a string message for no input for velocity', () => {});
+
+  // acceleration tests
+  test('accleration method should return 100 points for a 1.10 input', () => {});
+
+  test('accleration method should return a string message for no input for accleration', () => {});
+
+  // agility tests
+  test('', () => {});
+
+  test('agility method should return a string message for no input for agility', () => {});
+
+  // age tests
+  test('age method should return 50 points for a 20 input', () => {});
+
+  test('age method should return a string message for no input for age', () => {});
+
+  // size tests
+  test('size method should return 75 points for a 4.28 input', () => {});
+
+  test('size method should return a string message for no input for size', () => {});
+});
