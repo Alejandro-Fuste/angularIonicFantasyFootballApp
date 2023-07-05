6733daded8339de671a881d79499666c47d1cd56
Alejandro F <afuste7@gmail.com>
2023-07-05 15:14:59 -0400

    added tests for offense model in offense method file

--- test/offense/offense-method.test.ts
+++ test/offense/offense-method.test.ts
@@ -1,11 +1,59 @@
 import { describe, expect, test } from '@jest/globals';
+import { Offense } from '../../src/models/offense.model';
+
+let quarterback = 70;
+let offensiveCoordinator = 76;
+let offensiveLine = 78.8;
+let offenseSample = new Offense(
+  quarterback,
+  offensiveCoordinator,
+  offensiveLine
+);
+
+const testResults = {
+  sum: 224.8,
+};
 
 describe('grade model tests for offense methods', () => {
-  test('offense criteria total grade method returns a number grade', () => {});
+  test('return object has sum property that returns sum of 224.8', () => {
+    expect(gradeObject.sumCriteriaPoints(testInputs.array)).toBe(
+      testResults.sum
+    );
+  });
 
-  test('offense criteria method for quarterback returns a number grade', () => {});
+  test('return object has percentage property that returns 74.93', () => {
+    expect(gradeObject.sumCriteriaPoints(testInputs.array)).toBe(
+      testResults.sum
+    );
+  });
 
-  test('offense criteria method for offensive coordinator returns a number grade', () => {});
+  test('return object has letter property that returns C', () => {
+    expect(gradeObject.sumCriteriaPoints(testInputs.array)).toBe(
+      testResults.sum
+    );
+  });
 
-  test('offense criteria method for offensive line returns a number grade', () => {});
+  test('return object has description property that returns Average', () => {
+    expect(gradeObject.sumCriteriaPoints(testInputs.array)).toBe(
+      testResults.sum
+    );
+  });
+
+  test('return object has property for quarterback that returns a number grade', () => {
+    expect(gradeObject.sumCriteriaPoints(testInputs.array)).toBe(
+      testResults.sum
+    );
+  });
+
+  test('return object has property for offensive coordinator that returns a number grade', () => {
+    expect(gradeObject.sumCriteriaPoints(testInputs.array)).toBe(
+      testResults.sum
+    );
+  });
+
+  test('return object has property for offensive line returns a number grade', () => {
+    expect(gradeObject.sumCriteriaPoints(testInputs.array)).toBe(
+      testResults.sum
+    );
+  });
 });
