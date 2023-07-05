f7c7493fd6f0f1effc5d2d4fdb1eb2317fe3ba5b
Alejandro F <afuste7@gmail.com>
2023-07-05 15:17:57 -0400

    added variable and method to offense model file

--- src/models/offense.model.ts
+++ src/models/offense.model.ts
@@ -12,4 +12,33 @@ export class Offense extends Grade {
     this.offensive_coordinator = offensive_coordinator;
     this.offensive_line = offensive_line;
   }
+
+  array: Array<number> = [
+    this.quarterback,
+    this.offensive_coordinator,
+    this.offensive_line,
+  ];
+
+  possibleTotalPoints: number = 550;
+
+  returnObject(): {
+    letter: string;
+    description: string;
+    points: number;
+    percentage: number | string;
+  } {
+    let percentage = super.percentage(
+      super.sumCriteriaPoints(this.array),
+      this.possibleTotalPoints
+    );
+
+    let letterObject = super.criteriaGrade(percentage);
+
+    return {
+      points: super.sumCriteriaPoints(this.array),
+      percentage,
+      letter: letterObject.letter,
+      description: letterObject.description,
+    };
+  }
 }
