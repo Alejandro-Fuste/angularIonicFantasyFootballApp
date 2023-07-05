6831ad1e79bdefd6971d15d1172fb74d58975016
Alejandro F <afuste7@gmail.com>
2023-07-05 11:45:08 -0400

    removed unnecessary folder for opportunity

--- test/opportunity/opportunity-property.test.ts
+++
@@ -1,5 +0,0 @@
-import { describe, expect, test } from '@jest/globals';
-
-describe('grade model tests for offense properties', () => {
-  test('grade object has opportunity property', () => {});
-});
