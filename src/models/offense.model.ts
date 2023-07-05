37caf9d0ed32e9bcdba02dc57a5b3ee5bf735c50
Alejandro F <afuste7@gmail.com>
2023-07-05 10:54:02 -0400

    added code for property tests

--- src/models/offense.model.ts
+++ src/models/offense.model.ts
@@ -1,3 +1,15 @@
-export class Offense {
-  constructor() {}
+import { Grade } from './grade.model';
+
+export class Offense extends Grade {
+  constructor(
+    public quarterback: number,
+    public offensive_coordinator: number,
+    public offensive_line: number
+  ) {
+    super();
+
+    this.quarterback = quarterback;
+    this.offensive_coordinator = offensive_coordinator;
+    this.offensive_line = offensive_line;
+  }
 }
--- test/offense/offense-property.test.ts
+++ test/offense/offense-property.test.ts
@@ -1,11 +1,27 @@
 import { describe, expect, test } from '@jest/globals';
+import { Offense } from 'src/models/offense.model';
+
+let quarterback = 70;
+let offensiveCoordinator = 76;
+let offensiveLine = 78.8;
+let offenseSample = new Offense(
+  quarterback,
+  offensiveCoordinator,
+  offensiveLine
+);
 
 describe('grade model tests for offense properties', () => {
   test('grade object has offense property', () => {});
 
-  test('offense criteria property has quarterback property', () => {});
+  test('offense criteria property has quarterback property', () => {
+    expect(offenseSample.quarterback).toBe(quarterback);
+  });
 
-  test('offense criteria property has offensive coordinator property', () => {});
+  test('offense criteria property has offensive coordinator property', () => {
+    expect(offenseSample.offensive_coordinator).toBe(offensiveCoordinator);
+  });
 
-  test('offense criteria property has offensive line property', () => {});
+  test('offense criteria property has offensive line property', () => {
+    expect(offenseSample.offensive_line).toBe(offensiveLine);
+  });
 });
