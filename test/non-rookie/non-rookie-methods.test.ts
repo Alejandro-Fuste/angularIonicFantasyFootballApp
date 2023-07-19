2281da831010ad3b74c955ee7fd49e1f5c341d03
Alejandro F <afuste7@gmail.com>
2023-07-19 15:53:38 -0400

    added tests for array and property constructors

--- test/non-rookie/non-rookie-methods.test.ts
+++ test/non-rookie/non-rookie-methods.test.ts
@@ -73,3 +73,51 @@ const teSample = new NonRookie(
   receivingTouchdowns,
   receptions
 );
+
+describe('non-rookie model tests', () => {
+  // array constructor tests
+
+  test('array constructor method returns array with passing_touchdowns RB', () => {
+    expect(rbSample.arrayConstructor('RB')[6]).toBe(passingTouchdowns);
+  });
+
+  test('array constructor method returns array with receiving_touchdowns RB', () => {
+    expect(rbSample.arrayConstructor('RB')[7]).toBe(receivingTouchdowns);
+  });
+
+  // property value constructor tests
+
+  test('property value constructor method returns object with passing_yards for QB', () => {
+    expect(qbSample.returnObject().propertyValues.passing_yards).toBe(
+      passingYards
+    );
+  });
+
+  test('property value constructor method returns object with passing_touchdowns for QB', () => {
+    expect(qbSample.returnObject().propertyValues.passing_touchdowns).toBe(
+      passingTouchdowns
+    );
+  });
+
+  test('property value constructor method returns object with rushing_yards for RB', () => {
+    expect(rbSample.returnObject().propertyValues.rushing_yards).toBe(
+      rushingYards
+    );
+  });
+
+  test('property value constructor method returns object with targets for RB', () => {
+    expect(rbSample.returnObject().propertyValues.targets).toBe(targets);
+  });
+
+  test('property value constructor method returns object with receiving_yards for WR', () => {
+    expect(wrSample.returnObject().propertyValues.receiving_yards).toBe(
+      receivingYards
+    );
+  });
+
+  test('property value constructor method returns object with touchdowns for TE', () => {
+    expect(teSample.returnObject().propertyValues.receiving_touchdowns).toBe(
+      receivingTouchdowns
+    );
+  });
+});
