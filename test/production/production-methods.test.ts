15a10f3387c18bb551e3dac563612c049e4868cc
Alejandro F <afuste7@gmail.com>
2023-07-23 21:37:34 -0400

    added tests in production method file

--- test/production/production-methods.test.ts
+++ test/production/production-methods.test.ts
@@ -12,10 +12,35 @@ const rushingYards = 95;
 const rushingYards2 = 55;
 const receivingYards = 55;
 const receivingYards2 = 147;
-const touchdowns = 3;
-const touchdowns2 = 11;
-const passingTouchdowns = 15;
-const passingYards = 3015;
+const touchdowns = 75;
+const touchdowns2 = 95;
+const touchdowns3 = 147;
+const passingTouchdowns = 110;
+const passingYards = 127;
+
+let testObject = {
+  qbSum: 407,
+  rbSum: 300,
+  wrSum: 485,
+  teSum: 485,
+  qbPercentage: 61,
+  rbPercentage: 60,
+  wrPercentage: 97,
+  tePercentage: 97,
+  letters: ['A+', 'A', 'B+', 'B', 'C', 'D', 'F'],
+  descriptions: [
+    'Elite',
+    'Great',
+    'Good',
+    'Above Average',
+    'Average',
+    'Below Average',
+    'Not Good Enough',
+  ],
+  notAvailable: 'N/A',
+  notAvailableDescription:
+    'Oops, something went wrong..could not calculate grade',
+};
 
 const qbSample = new Production(
   position,
@@ -43,7 +68,7 @@ const wrSample = new Production(
   rushingYards2,
   receivingYards2,
   undefined,
-  touchdowns2,
+  touchdowns3,
   receptions2
 );
 
@@ -53,7 +78,7 @@ const teSample = new Production(
   rushingYards2,
   receivingYards2,
   undefined,
-  touchdowns2,
+  touchdowns3,
   receptions2
 );
 
@@ -102,5 +127,79 @@ describe('production model tests', () => {
   test('property value constructor method returns object with touchdowns for TE', () => {
     console.log(teSample.returnObject());
     expect(teSample.returnObject().propertyValues.touchdowns).toBe(touchdowns2);
+  });
+
+  // return object tests
+
+  test('letter property for QB should be D', () => {
+    expect(qbSample.returnObject().letter).toBe(testObject.letters[5]);
+  });
+
+  test('letter property for RB should be D', () => {
+    expect(rbSample.returnObject().letter).toBe(testObject.letters[5]);
+  });
+
+  test('letter property for WR should be A+', () => {
+    expect(wrSample.returnObject().letter).toBe(testObject.letters[0]);
+  });
+
+  test('letter property for TE should be A+', () => {
+    expect(teSample.returnObject().letter).toBe(testObject.letters[0]);
+  });
+
+  test('description property for QB should be below average', () => {
+    expect(qbSample.returnObject().description).toBe(
+      testObject.descriptions[5]
+    );
+  });
+
+  test('description property for RB should be below average', () => {
+    expect(rbSample.returnObject().description).toBe(
+      testObject.descriptions[5]
+    );
+  });
+
+  test('description property for WR should be elite', () => {
+    expect(wrSample.returnObject().description).toBe(
+      testObject.descriptions[0]
+    );
+  });
+
+  test('description property for TE should be elite', () => {
+    expect(teSample.returnObject().description).toBe(
+      testObject.descriptions[0]
+    );
+  });
+
+  test('points property for QB should be 407', () => {
+    expect(qbSample.returnObject().points).toBe(testObject.qbSum);
+  });
+
+  test('points property for RB should be 300', () => {
+    expect(rbSample.returnObject().points).toBe(testObject.rbSum);
+  });
+
+  test('points property for WR should be 485', () => {
+    expect(wrSample.returnObject().points).toBe(testObject.wrSum);
+  });
+
+  test('points property for TE should be 485', () => {
+    expect(teSample.returnObject().points).toBe(testObject.teSum);
+  });
+
+  test('percentage property for QB should be 61', () => {
+    expect(qbSample.returnObject().percentage).toBe(testObject.qbPercentage);
+  });
+
+  test('percentage property for RB should be 60', () => {
+    expect(rbSample.returnObject().percentage).toBe(testObject.rbPercentage);
+  });
+
+  test('percentage property for TE should be 97', () => {
+    expect(wrSample.returnObject().percentage).toBe(testObject.wrPercentage);
+  });
+
+  test('percentage property for TE should be 97', () => {
+    expect(teSample.returnObject().percentage).toBe(testObject.tePercentage);
   });
 });
