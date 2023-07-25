56368f46080e4dc372c34e84bf9485ef5601491f
Alejandro F <afuste7@gmail.com>
2023-07-25 12:33:54 -0400

    added code for qb methods in productionCalculator file

--- src/utils/productionCalculator.ts
+++ src/utils/productionCalculator.ts
@@ -17,19 +17,63 @@ export default {
         return 0;
       }
     },
-    rushing_yards: (value) => {},
-    passing_touchdowns: (value) => {},
-    rushing_touchdowns: (value) => {},
+    rushing_yards: (value: number): number => {
+      if (value >= 1000) {
+        return 166;
+      } else if (value >= 900 && value <= 999) {
+        return 156;
+      } else if (value >= 800 && value <= 899) {
+        return 146;
+      } else if (value >= 700 && value <= 799) {
+        return 136;
+      } else if (value >= 600 && value <= 699) {
+        return 126;
+      } else if (value >= 400 && value <= 599) {
+        return 116;
+      } else {
+        return 0;
+      }
+    },
+    passing_touchdowns: (value: number): number => {
+      if (value >= 48) {
+        return 167;
+      } else if (value >= 40 && value <= 47) {
+        return 157;
+      } else if (value >= 30 && value <= 39) {
+        return 147;
+      } else if (value >= 20 && value <= 29) {
+        return 137;
+      } else if (value >= 10 && value <= 19) {
+        return 110;
+      } else {
+        return 0;
+      }
+    },
+    rushing_touchdowns: (value: number): number => {
+      if (value >= 12) {
+        return 167;
+      } else if (value >= 10 && value <= 11) {
+        return 157;
+      } else if (value >= 8 && value <= 9) {
+        return 147;
+      } else if (value >= 6 && value <= 7) {
+        return 137;
+      } else if (value >= 2 && value <= 4) {
+        return 110;
+      } else {
+        return 0;
+      }
+    },
   },
   RB: {
-    rushing_yards: (value) => {},
-    receiving_yards: (value) => {},
-    touchdowns: (value) => {},
-    receptions: (value) => {},
+    rushing_yards: (value: number): number => {},,
+    receiving_yards: (value: number): number => {},,
+    touchdowns: (value: number): number => {},,
+    receptions: (value: number): number => {},,
   },
   WR_TE: {
-    receiving_yards: (value) => {},
-    touchdowns: (value) => {},
-    receptions: (value) => {},
+    receiving_yards: (value: number): number => {},,
+    touchdowns: (value: number): number => {},,
+    receptions: (value: number): number => {},,
   },
 };
