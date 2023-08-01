fe6b859144dbcc038932e341747c4edc5f71f588
Alejandro F <afuste7@gmail.com>
2023-07-31 23:31:49 -0400

    added code for getting player stats from api

---
+++ src/data/players/players.js
@@ -0,0 +1,15 @@
+/*
+
+    "sportradar_id": "1111"
+    “first_name”:  “John”,
+		“last_name”:  “Doe”,
+		“position”: “QB”,
+		“team”: “NE”,
+		“age”: 21,
+		“height”: “6’4\””,
+		“weight”: “220”
+		“years_exp”: 14,
+		“depth_chart_order”: 1
+    "grades": {}
+
+*/
